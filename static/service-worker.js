7eb35abe2b2e95563d04b2c035ee703486b6ab51
fernando calvente salas <fernandocalventesalas@MacBook-Pro-de-fernando.local>
2026-02-14 03:45:23 +0100

    Preparado para producción y Render

--- static/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', function(event) {
-  // Esto permite que la app siga funcionando offline para requests simples
-});
